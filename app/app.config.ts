b3433a73d30f7ecfd59f4bc2c45db53f13c9fb37
Ghriver <2025650064@qq.com>
2026-02-26 03:59:22 +0000

    update app/app.config.ts.
    Signed-off-by: Ghriver <2025650064@qq.com>

--- app/app.config.ts
+++ app/app.config.ts
@@ -45,9 +45,9 @@ export default defineAppConfig({
 
 		stats: {
 			/** 归档页面每年标题对应的年龄 */
-			birthYear: 2003,
+			birthYear: 2011,
 			/** blog-stats widget 的预置文本 */
-			wordCount: '约10万',
+			wordCount: '约0.1万',
 		},
 	},
 
@@ -93,11 +93,11 @@ export default defineAppConfig({
 
 	/** 左侧栏顶部 Logo */
 	header: {
-		logo: 'https://weavatar.com/avatar/47c0f2e82b76d9b10eb3023df9e02e4e3fdbeaf5b74b842063f207971e7fbe7b?s=160',
+		logo: 'https://file.catp.cc/avatar.png',
 		/** 展示标题文本，否则展示纯 Logo */
 		showTitle: true,
 		subtitle: blogConfig.subtitle,
-		emojiTail: ['📄', '🦌', '🙌', '🐟', '🏖️'],
+		emojiTail: ['🎉', '🎋', '🐲', '🍁', '🇨🇳'],
 	},
 
 	/** 友链页面 */
