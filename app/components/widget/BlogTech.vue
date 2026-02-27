bdade0070ad664a66072c1285b2f84eda586445b
Ghriver <2025650064@qq.com>
2026-02-27 14:51:42 +0000

    1
    Signed-off-by: Ghriver <2025650064@qq.com>

--- app/components/widget/BlogTech.vue
+++ app/components/widget/BlogTech.vue
@@ -23,7 +23,7 @@ const [pm, pmVersion] = packageManager.split('@') as [string, string]
 
 const service = computed(() => ([
 	...ci ? [{ label: '构建平台', value: ciPlatform }] : [],
-	{ label: '图片存储', value: () => [h(Icon, { name: 'simple-icons:cloudflare', alt: '' }), ' R2'] },
+	{ label: '图片存储', value: () => [h(Icon, { name: 'logos:aws-s3', alt: '' }), ' MoePic Maple Next(Via S3)'] },
 	{ label: '软件协议', value: 'MIT' },
 	{ label: '文章许可', value: appConfig.copyright.abbr },
 	{ label: '规范域名', value: getDomain(appConfig.url) },
